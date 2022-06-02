3401fd0187e363a3ed853ce5ba94e9d6bdbff621
muslimitmilch <97620732+muslimitmilch@users.noreply.github.com>
2022-06-02 14:53:48 +0200

    bug fixes in advancedDisplay
    also removed legacy terminalDisplay

--- minceraft/terminalDisplay.py
+++ minceraft/terminalDisplay.py
@@ -1,75 +1,5 @@
 import os, math
 
-class terminalDisplay:
-	
-    def __init__(self):
-        self.outputList = []
-        self.logo = True
-        self.longSpacer = "                                                                                                                                                                                                                              "
-		
-    def __del__(self):
-        pass
-        #os.system('clear')
-	
-    def clear(self):
-        pass # selection for debugging purposeseses
-        #os.system('clear')
-			
-    def set(self, input):
-        if type(input) is str:
-            self.outputList = [input]
-        else:
-            self.outputList = input
-        self.display()
-			
-    def appendTop(self, lineString):
-        self.outputList.reverse()
-        self.outputList.append(lineString)
-        self.outputList.reverse()
-        self.display()	
-		
-    def append(self, appendObject):
-        if(type(appendObject) is str):
-            self.outputList.append(appendObject)
-        else:
-            self.outputList += appendObject
-        self.display()
-        
-	
-    def userInput(self):
-        return input('    ')
-        
-    def display(self):
-        if(self.logo):
-            terminalWidth, terminalLength = os.get_terminal_size(0)
-            terminalLength -= 1
-            #logoFile = open('logo.txt', 'r')
-            logoLines = ["    __  ____                            ______ ",
-"   /  |/  (_)___  ________  _________ _/ __/ /_",
-"  / /|_/ / / __ \/ ___/ _ \/ ___/ __ `/ /_/ __/",
-" / /  / / / / / / /__/  __/ /  / /_/ / __/ /_  ",
-"/_/  /_/_/_/ /_/\___/\___/_/   \____/_/  \__/  "] #logoFile.readlines()
-            #logoFile.close()
-            self.clear()
-            if(0 > (terminalLength - (math.ceil(terminalLength / 2)) - len(logoLines) - len(self.outputList))):
-                for i in range(terminalLength - len(self.outputList)):
-                    print()
-            else:
-                self.spacerString = self.longSpacer[:int((terminalWidth - 48) / 2)]
-                for i in range (math.ceil(terminalLength / 2) - len(logoLines)):
-                    print()
-                for i in logoLines:
-                    print((self.spacerString + i)[:-1])
-                for i in range(math.floor(terminalLength / 2) - len(self.outputList)):
-                    print()
-            for i in self.outputList:
-                print('    ' + i)
-
-		
-		
-		
-		
-		
 class advancedDisplay():
 
     def __init__(self):
@@ -108,14 +38,13 @@ class advancedDisplay():
         self.output()
         
     def homeSet(self, homeObject, homeLength = 5):
-        self.homeLength = homeLength
         self.homeList = ['']
+        self.homeLength = homeLength + 2
         if(type(homeObject) is str):
             self.homeList += [homeObject]
         else:
             self.homeList += homeObject
         self.homeList.append('')
-        self.homeList.reverse()
         self.output()
 
     def output(self):
@@ -126,19 +55,6 @@ class advancedDisplay():
 " / /  / / / / / / /__/  __/ /  / /_/ / __/ /_  ",
 "/_/  /_/_/_/ /_/\___/\___/_/   \____/_/  \__/  "]
         os.system('clear')
-        if(terminalLength > (self.homeLength + len(self.outputList) + len(logoLines) + (terminalLength / 2))):
-            for i in range(math.ceil(terminalLength/2) - len(logoLines)):
-                print()
-            spacer = self.longSpacer[:math.floor((terminalWidth - 48) / 2)]
-            for i in logoLines:
-                print(spacer, i)
-            for i in range(math.floor(terminalLength / 2) - len(self.outputList) - self.homeLength):
-                print()
-        else:
-            for i in range(terminalLength - self.homeLength - len(self.outputList)):
-                print()
-        for i in self.outputList:
-            print('   ' + i)
         self.homeList.reverse()
         while(True):
             if(len(self.homeList) < self.homeLength):
@@ -146,6 +62,19 @@ class advancedDisplay():
             else:
                 break
         self.homeList.reverse()
+        if(terminalLength > (len(self.homeList) + len(self.outputList) + len(logoLines) + (terminalLength / 2))):
+            for i in range(math.ceil(terminalLength/2) - len(logoLines) - 1):
+                print()
+            spacer = self.longSpacer[:math.floor((terminalWidth - 48) / 2)]
+            for i in logoLines:
+                print(spacer, i)
+            for i in range(math.floor(terminalLength / 2) - len(self.outputList) - len(self.homeList)):
+                print()
+        else:
+            for i in range(terminalLength - self.homeLength - len(self.outputList)):
+                print()
+        for i in self.outputList:
+            print('   ' + i)
         for i in self.homeList:
             print('    ' + i)
             
